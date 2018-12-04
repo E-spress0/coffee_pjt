4d84919894eebaddf91952ff5ef3858382e2eea7
Espresso <cafemocha4437@gmail.com>
2018-12-04 15:01:59 +0900

    게시판 페이징 기능 수정(최근 올라간 게시물 부터 출력)

--- coffee_admin/src/main/java/com/coffee_admin/www/model/board/BoardModel.java
+++ coffee_admin/src/main/java/com/coffee_admin/www/model/board/BoardModel.java
@@ -30,25 +30,35 @@ public class BoardModel {
 	/** 마지막 페이지 번호 */
 	private int lastPage;
 
+	/** 시작 페이지 */
 	private int startPage = 1;
+
+	/** 끝 페이지 */
 	private int endPage = 1;
 
+	/** 게시물 시작 번호 */
 	private int startBoard = 1;
+
+	/** 게시물 끝 번호 */
 	private int endBoard = 1;
 
+	/** 이전 페이지 */
 	private int prevPage;
+
+	/** 다음 페이지 */
 	private int nextPage;
 
 	public BoardModel(int listCnt, int nowPage) {
 		setListCnt(listCnt);
-		setNowPage(nowPage);
 		setLastPage(listCnt);
+		//		setNowPage(this.lastPage - nowPage + 1);
+		setNowPage(nowPage);
 		if (nowPage > 10) {
 			setStartPage(nowPage);
 		}
 		setEndPage(nowPage);
-		setStartBoard(nowPage);
-		setEndBoard(listCnt, nowPage);
+		setStartBoard();
+		setEndBoard(listCnt);
 	}
 
 	public void setListCnt(int listCnt) {
@@ -64,25 +74,40 @@ public class BoardModel {
 	}
 
 	public void setStartPage(int nowPage) {
-		this.startPage = nowPage / pageSize * pageSize + 1;
+		this.startPage = (nowPage - 1) / pageSize * pageSize + 1;
 	}
 
 	public void setEndPage(int nowPage) {
-		if (nowPage > 10 && lastPage > 10) {
-			this.endPage = ((nowPage / 10 * 10 + 10) >= lastPage) ? lastPage : nowPage / 10 * 10 + 10;
-		} else if (nowPage > 0 && lastPage > 10) {
-			this.endPage = 10;
-		} else {
+		/*if (nowPage > 10 && lastPage > 10) {*/
+		if (lastPage > 10) {
+			this.endPage = (((nowPage - 1) / 10 * 10 + 10) >= lastPage) ? lastPage : (nowPage - 1) / 10 * 10 + 10;
+		} /*else if (nowPage > 0 && lastPage > 10) {
+		  this.endPage = 10;
+		  }*/ else {
 			this.endPage = lastPage;
 		}
 	}
 
-	public void setStartBoard(int nowPage) {
-		this.startBoard = (nowPage - 1) * board_Size + 1;
+	/*public void setStartBoard() {
+		this.startBoard = (this.nowPage - 1) * board_Size + 1;
+	}
+	
+	public void setEndBoard(int listCnt) {
+		this.endBoard = ((this.nowPage - 1) * board_Size + 10 <= listCnt) ? (this.nowPage - 1) * board_Size + 10
+		        : listCnt;
+	}*/
+
+	public void setStartBoard() {
+		this.startBoard = this.listCnt - ((this.nowPage - 1) * board_Size + 9);
+		if (this.startBoard < 0) {
+			this.startBoard = 0;
+		}
 	}
 
-	public void setEndBoard(int listCnt, int nowPage) {
-		this.endBoard = ((nowPage - 1) * board_Size + 10 <= listCnt) ? (nowPage - 1) * board_Size + 10 : listCnt;
+	public void setEndBoard(int listCnt) {
+		this.endBoard = this.listCnt - ((this.nowPage - 1) * board_Size);
+		/*if (this.startBoard == 0) {
+			this.endBoard = 0;
+		}*/
 	}
-
 }
